69b9f2084f602f41f75505cb5a7f3767c0d95d87
Andy Gonzalez <agonzalezjr@gmail.com>
2020-10-06 13:54:08 -0400

    dynamic edge example

--- src/tests/basic-workflow-elements.ts
+++ src/tests/basic-workflow-elements.ts
@@ -7,10 +7,26 @@ export default [
   { data: { id: "Completed", label: "Completed" }, selectable: true },
   { data: { id: "Approved", label: "Approved" }, selectable: true },
   // edges
-  { data: { source: "Created", target: "Assigned", label: "assign" } },
-  { data: { source: "Assigned", target: "Started", label: "start" } },
-  { data: { source: "On Hold", target: "Started", label: "start" } },
-  { data: { source: "Started", target: "On Hold", label: "hold" } },
-  { data: { source: "Started", target: "Completed", label: "complete" } },
-  { data: { source: "Completed", target: "Approved", label: "approve" } },
+  {
+    data: { id: "a1", source: "Created", target: "Assigned", label: "assign" },
+  },
+  { data: { id: "a2", source: "Assigned", target: "Started", label: "start" } },
+  { data: { id: "a3", source: "On Hold", target: "Started", label: "start" } },
+  { data: { id: "a4", source: "Started", target: "On Hold", label: "hold" } },
+  {
+    data: {
+      id: "a5",
+      source: "Started",
+      target: "Completed",
+      label: "complete",
+    },
+  },
+  {
+    data: {
+      id: "a6",
+      source: "Completed",
+      target: "Approved",
+      label: "approve",
+    },
+  },
 ];
